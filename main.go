a7072854a6be2e1e4bebc3bc0b31f8d2c3a7ebec
Akhil <1271akhil@gmail.com>
2022-12-03 01:22:34 -0500

    Add friends birthday to the user

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"log"
 	"net/http"
 	"time"
@@ -19,26 +21,33 @@ import (
 //			video games
 
 type User struct {
-	ID              int64      `json:"ID"`
-	FirstName       string     `json:"firstName"`
-	LastName        string     `json:"lastName"`
+	ID              int64      `json:"id"`
+	FirstName       string     `json:"firstName" validate:"required,alpha,min=3,max=50"`
+	LastName        string     `json:"lastName" validate:"required,alpha,min=1,max=50"`
+	Email           string     `json:"email" validate:"required,email"`
 	FriendsBirthday []Birthday `json:"friendsBirthday"`
-	Email           string     `json:"email"`
 }
 
 type Birthday struct {
-	ID         int64     `json:"ID"`
-	Name       string    `json:"name"`
-	DOB        time.Time `json:"DOB"`
-	Gender     string    `json:"gender"`
-	ProfilePic string    `json:"profilePic"`
-	Interests  []int64   `json:"interests"`
+	UniqueIdentifier string    `json:"uniqueIdentifier"`
+	ID               int64     `json:"id"`
+	FirstName        string    `json:"firstName" validate:"required,alpha,min=3,max=50"`
+	LastName         string    `json:"lastName" validate:"required,alpha,min=1,max=50"`
+	DateOfBirth      time.Time `json:"dateOfBirth" validate:"isValidDOB"`
+	Age              int       `json:"age"`
+	Gender           string    `json:"gender" validate:"required,oneof=MALE FEMALE"`
+	ProfilePic       string    `json:"profilePic"`
+	Interests        []int64   `json:"interests"`
 }
 
 type Interest struct {
-	ID       int64  `json:"ID"`
+	ID       int64  `json:"id"`
 	Name     string `json:"name"`
 	Category string `json:"category"`
+}
+
+type EmailParam struct {
+	Email string `uri:"email" validate:"required,email"`
 }
 
 var (
@@ -61,11 +70,23 @@ var (
 
 var users = make(map[string]User)
 
+var fieldValidator *validator.Validate
+
+func init() {
+	fieldValidator = validator.New()
+
+	fieldValidator.RegisterValidation("isValidDOB", func(fl validator.FieldLevel) bool {
+		t := fl.Field().Interface().(time.Time)
+		return t.Before(time.Now())
+	})
+}
+
 func main() {
 	server := gin.New()
-	server.POST("user", func(context *gin.Context) {
+	// creating the user
+	server.POST("users", func(context *gin.Context) {
 		var user User
-		//binding the json
+		//binding the json (converting postman json body to golang struct)
 		err := context.ShouldBindJSON(&user)
 		if err != nil {
 			log.Println("Error while creating the user")
@@ -74,9 +95,24 @@ func main() {
 			})
 			return
 		}
+		// manual validations performmed below
+		//if user.FirstName == "" || user.LastName == "" || user.Email == "" {
+		//	context.JSON(http.StatusBadRequest, map[string]string{
+		//		"message": "required fields are missing",
+		//	})
+		//	return
+		//}
+
+		err = fieldValidator.Struct(user)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, map[string]string{
+				"message": err.Error(),
+			})
+			return
+		}
 
 		//check the user is already Exits
-		user, isExits := users[user.Email]
+		_, isExits := users[user.Email]
 		if isExits {
 			context.JSON(http.StatusConflict, map[string]string{
 				"message": "user already exists",
@@ -90,8 +126,84 @@ func main() {
 			"message": "success",
 		})
 	})
+
+	//Getting all the users
+	server.GET("users", func(context *gin.Context) {
+		context.JSON(http.StatusOK, map[string]interface{}{
+			"users": users,
+		})
+	})
+
+	//Adding friends birthday to the user
+	server.POST("/users/:email/birthday", func(context *gin.Context) {
+		//bind the uri with a golang structure
+		var emailParam EmailParam
+		err := context.ShouldBindUri(&emailParam)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"msg": err.Error()})
+			return
+		}
+
+		//do field level validations for URI structure (fieldValidator.Struct(email))
+		err = fieldValidator.Struct(emailParam)
+		if err != nil {
+			log.Println("Error while validating the email", err)
+			context.JSON(http.StatusBadRequest, map[string]string{
+				"message": "Email required",
+			})
+			return
+		}
+
+		//bind the request body(birthday) with a golang structure
+		var birthday Birthday
+		err = context.ShouldBindJSON(&birthday)
+		if err != nil {
+			log.Println("Error while creating the user")
+			context.JSON(http.StatusBadRequest, map[string]string{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		//do field level validations for request body structure
+		err = fieldValidator.Struct(birthday)
+		if err != nil {
+			log.Println("Error while validating the email", err)
+			context.JSON(http.StatusBadRequest, map[string]string{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		user, isExits := users[emailParam.Email]
+		if !isExits {
+			context.JSON(http.StatusBadRequest, map[string]string{
+				"message": "Email not found",
+			})
+			return
+		}
+		for _, existingBirthday := range user.FriendsBirthday {
+			if existingBirthday.UniqueIdentifier == fmt.Sprintf("%s_%s_%s", birthday.FirstName, birthday.LastName, birthday.Gender) {
+				context.JSON(http.StatusConflict, map[string]string{
+					"message": "birthday already found",
+				})
+				return
+			}
+		}
+		birthday.UniqueIdentifier = fmt.Sprintf("%s_%s_%s", birthday.FirstName, birthday.LastName, birthday.Gender)
+		birthday.ID = int64(len(user.FriendsBirthday) + 1)
+		user.FriendsBirthday = append(user.FriendsBirthday, birthday)
+		users[user.Email] = user
+
+		context.JSON(http.StatusOK, map[string]string{
+			"message": "Friends birthday added successfully",
+		})
+
+	})
+
 	err := server.Run()
 	if err != nil {
 		log.Println("Unable to start server: ", err)
 	}
+
 }
